feb0b4c51e36492cc24f94c5a2d4e6303f197182
Xander Cesari <xander@merriman.industries>
2026-01-11 18:02:55 -0800

    Added basic flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A can-dbc LSP written in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            # zsh
+            # pnpm
+            nodejs_24
+            tree-sitter
+          ];
+
+          shellHook = '''';
+        };
+      }
+
+    );
+}
